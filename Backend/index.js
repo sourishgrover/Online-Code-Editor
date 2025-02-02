2eb6f690ce2e69eeb2c7ade4cb27abe637931eb5
sourishgrover <sourishgrover50@gmail.com>
2025-02-02 19:50:24 +0530

    first

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
+import axios from "axios";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import path from "path";
 
 const app = express();
 
 const server = http.createServer(app);
+
+
 
 const io = new Server(server, {
   cors: {
@@ -40,11 +44,89 @@ io.on("connection", (socket) => {
 
     io.to(roomId).emit("userJoined", Array.from(rooms.get(currentRoom)));
     console.log("user joined room",roomId);
+
+    socket.on("codeChange",({roomId,code})=>{
+      socket.to(roomId).emit("codeUpdate",code);
+    });
+
+
+    socket.on("leaveRoom",()=>{
+      if(currentRoom && currentUser){
+        rooms.get(currentRoom).delete(currentUser)
+        io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+
+        socket.leave(currentRoom)
+
+        currentRoom = null;
+        currentUser = null;
+    }
+  })
+
+  socket.on("typing", ({ roomId, userName }) => {
+    socket.to(roomId).emit("userTyping", userName);
+  });
+
+
+
+  socket.on("languageChange",({roomId , language})=>{
+    io.to(roomId).emit("languageUpdate",language)
+  })
+    socket.on("disconnect",()=>{
+      if(currentRoom && currentUser){
+        rooms.get(currentRoom).delete(currentUser)
+        io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+        console.log("user Disconnected");
+        
+      }
+    })
+
+    socket.on("compileCode", async ({ code, roomId, language, version }) => {
+      if (rooms.has(roomId)) {
+        try {
+          const response = await axios.post("https://emkc.org/api/v2/piston/execute", {
+            language: language,
+            version: version,
+            files: [
+              {
+                content: code, 
+              },
+            ],
+          });
+    
+         
+          io.to(roomId).emit("codeResponse", {
+            success: true,
+            output: response.data.run.output,
+          });
+        } catch (error) {
+          console.error("Compilation error:", error);
+          io.to(roomId).emit("codeResponse", {
+            success: false,
+            error: error.message,
+          });
+        }
+      } else {
+        console.error("Room not found:", roomId);
+        io.to(roomId).emit("codeResponse", {
+          success: false,
+          error: "Room not found",
+        });
+      }
+    });
+    
     
   });
 });
 
 const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   console.log("server is working on port 5000");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
+import Editor from "@monaco-editor/react";
 
 const socket = io("http://localhost:5000");
 
@@ -8,16 +9,98 @@ const App = () => {
   const [joined, setJoined] = useState(false);
   const [userName, setUserName] = useState(" ");
   const [roomId, setRoomId] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const [code, setCode] = useState("// start Code here");
+  const [copySucess, setCopySucess] = useState("");
+  const [users, setUsers] = useState([]);
+  const [typing, setTyping] = useState("");
+  const [output, setOutput] = useState("");
+  const [version, setVersion] = useState("*");
 
+  useEffect(() => {
+    socket.on("userJoined", (users) => {
+      setUsers(users);
+    });
 
-  const joinRoom = ()=>{
-         if(roomId && userName){
-          socket.emit("join",{roomId, userName})
-          setJoined(true)
+    socket.on("codeUpdate", (newCode) => {
+      setCode(newCode);
+    });
 
-         }
-         
-  }
+    socket.on("userTyping", (user) => {
+      setTyping(`${user.slice(0, 8)}... is Typing`);
+      setTimeout(() => setTyping(""), 2000);
+    });
+
+    socket.on("languageUpdate", (newLanguage) => {
+      setLanguage(newLanguage);
+    });
+
+    socket.on("codeResponse", (response) => {
+      if (response.success) {
+        setOutput(response.output); 
+      } else {
+        setOutput(`Error: ${response.error}`); 
+      }
+    });
+
+    return () => {
+      socket.off("userJoined");
+      socket.off("codeUpdate");
+      socket.off("userTyping");
+      socket.off("languageUpdate");
+      socket.off("codeResponse");
+    };
+  }, []);
+
+  useEffect(() => {
+    const handelBeforeUnload = () => {
+      socket.emit("leaveRoom");
+    };
+
+    window.addEventListener("beforeunload", handelBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handelBeforeUnload);
+    };
+  }, []);
+
+  const joinRoom = () => {
+    if (roomId && userName) {
+      socket.emit("join", { roomId, userName });
+      setJoined(true);
+    }
+  };
+
+  const leaveRoom = () => {
+    socket.emit("leaveRoom");
+    setJoined(false);
+    setRoomId("");
+    setUserName("");
+    setCode("// start Code here");
+    setLanguage("javascript");
+  };
+
+  const copyRoomId = () => {
+    navigator.clipboard.writeText(roomId);
+    setCopySucess("Copied");
+    setTimeout(() => setCopySucess(""), 2000);
+  };
+
+  const handelCodeChange = (newCode) => {
+    setCode(newCode);
+    socket.emit("codeChange", { roomId, code: newCode });
+    socket.emit("typing", { roomId, userName });
+  };
+
+  const handelLanguageChange = (e) => {
+    const newLanguage = e.target.value;
+    setLanguage(newLanguage);
+    socket.emit("languageChange", { roomId, language: newLanguage });
+  };
+
+  const runCode = () => {
+    socket.emit("compileCode", { code, roomId, language, version });
+  };
 
   if (!joined) {
     return (
@@ -41,7 +124,69 @@ const App = () => {
       </div>
     );
   } else {
-    return <div>App</div>;
+    return (
+      <div className="edit-container">
+        <div className="sidebar">
+          <div className="room-info">
+            <h2>Code Room: {roomId}</h2>
+            <button onClick={copyRoomId} className="copy-button">
+              {" "}
+              Copy Id
+            </button>
+            {copySucess && <span>{copySucess}</span>}
+          </div>
+          <h3>User in Room</h3>
+          <ul>
+            {users.map((users, index) => (
+              <li key={index}>{users.slice(0, 8)}...</li>
+            ))}
+          </ul>
+          <p className="typing-indicator">{typing}</p>
+
+          <select
+            className="language-selector"
+            value={language}
+            onChange={handelLanguageChange}
+          >
+            <option value="javascript">JavaScript</option>
+            <option value="java">Java</option>
+            <option value="python">Python</option>
+            <option value="cpp">C++</option>
+          </select>
+          <button onClick={leaveRoom} className="leave-button">
+            Leave Room
+          </button>
+        </div>
+
+        <div className="editor-wrapper">
+          <Editor
+            height={"60%"}
+            width={"100%"}
+            defaultLanguage={language}
+            language={language}
+            value={code}
+            onChange={handelCodeChange}
+            theme="vs-dark"
+            options={{
+              minimap: { enabled: false },
+              fontSize: 14,
+              suggest: {
+                enabled: true,
+              },
+            }}
+          />
+          <button className="runBtn" onClick={runCode}>
+            Execute
+          </button>
+          <textarea
+            className="output-console"
+            value={output}
+            readOnly
+            placeholder="Your output will appear here..."
+          ></textarea>
+        </div>
+      </div>
+    );
   }
 };
 
